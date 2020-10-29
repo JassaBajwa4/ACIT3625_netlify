e536082b5f6ee5b84efa3850b71ce27acbfe531c
jass <jassabajwa4@gmail.com>
2020-10-29 13:25:26 -0700

    Updates

---
+++ precache-manifest.a4683f4ad0276dead2baa183624c179f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14ef0e0605c306a03c5625067e6d2bf7",
+    "url": "/ACIT3625_netlify/index.html"
+  },
+  {
+    "revision": "52adb00e45429aba0205",
+    "url": "/ACIT3625_netlify/static/css/main.d88b8004.chunk.css"
+  },
+  {
+    "revision": "cc9d43f972428e1b4cd8",
+    "url": "/ACIT3625_netlify/static/js/2.43573772.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ACIT3625_netlify/static/js/2.43573772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52adb00e45429aba0205",
+    "url": "/ACIT3625_netlify/static/js/main.c9af536e.chunk.js"
+  },
+  {
+    "revision": "c477c4dfb1999c00e0d2",
+    "url": "/ACIT3625_netlify/static/js/runtime-main.6c5df7db.js"
+  }
+]);
